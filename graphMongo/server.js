f9550c26796e6a88d58ceeeebda771afe4ed6f4c
yashar6846 <yasharnajafi6846@gmail.com>
2022-02-13 20:14:38 +0300

    add users

--- graphMongo/server.js
+++ graphMongo/server.js
@@ -5,7 +5,9 @@ import { quotes, users } from "./fakedb.js";
 const typeDefs = gql`
   type Query {
     users: [User]
+    user(id: ID!): User
     quotes: [Quote]
+    iquote(by: ID!): [Quote]
   }
   type User {
     id: ID!
@@ -24,7 +26,9 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     users: () => users,
+    user: (_, { id }) => users.find((user) => user.id == id),
     quotes: () => quotes,
+    iquote: (_, { by }) => quotes.filter((quote) => quote.by == by),
   },
   User: {
     quotes: (ur) => quotes.filter((quote) => quote.by == ur.id),
